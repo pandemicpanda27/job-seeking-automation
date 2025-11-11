b0d63a7e0e9878fa28753a1951afe1a6e4d533a4
Chirag Srivastava <chiragsrivastava626@gmail.com>
2025-11-11 23:29:53 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from application import create_app
-from application.insert_csv import insert_csv_to_mysql
-
 
 app = create_app()
 
 if __name__ == '__main__':
-    # insert_csv_to_mysql('Job Listings.csv')
-    app.run(debug = True)
- 
+    print("\n" + "="*60)
+    print(" Job Seeking Automation - Starting...")
+    print("="*60)
+    print(" Access at: http://localhost:5000")
+    print(" Press CTRL+C to stop")
+    print("="*60 + "\n")
+    app.run(debug=True)
